c0642f79db57a59e5494f99b1341c5d48691d03a
eddie <edwardobande36@gmail.com>
2019-08-12 07:20:56 +0100

    changePassword: fixed change password

--- .history/car-app/src/redux/reducers/index_20190810083300.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import ReportReducer from './ReportReducer';
-
-export default rootReducer = combineReducers({
- ReportReducer,
-});
--- .history/car-app/src/redux/reducers/index_20190811202129.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import ReportReducer from './ReportReducer';
-import ProfileReducer from './ProfileReducer';
-
-export default rootReducer = combineReducers({
- ReportReducer, ProfileReducer
-});
